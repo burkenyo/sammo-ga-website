f752ae83fa097b7eb4b341e92ee7ea010cc78247
Sammo Gabay <sammolito@gmail.com>
2022-12-25 10:59:54 -0500

    Add build and deploy action for Azure Static Web App

--- scripts/scaffold-mocks.ts
+++ scripts/scaffold-mocks.ts
@@ -1,19 +1,25 @@
 // scaffold-mocks.ts
 // handles scaffolding any files that are needed for mocking services during local testing
 
+/// <reference types="../global" />
+
 import * as fs from "node:fs";
 import path from "node:path";
 
-import { OeisFractionalExpansion } from "@/oeis";
+import { OeisId } from "@/oeis";
 
 // Create the list of expansions used by the MockApiRunner
 const idStrings = fs.readdirSync("public")
   .filter(f => f.endsWith(".txt"))
   .map(f => {
-    const content = fs.readFileSync(path.join("public", f), { encoding: "utf-8" });
-    const expansion = OeisFractionalExpansion.parseRawText(content);
-
-    return String(expansion.id);
-  });
+    try {
+      const line = fs.readFileSync(path.join("public", f), { encoding: "utf-8" })
+        .split(/\r?\n/g, 1)[0];
+      return String(OeisId.parse(line));
+    } catch {
+      return null;
+    }
+  })
+  .filter(i => i)
 
 fs.writeFileSync(path.join("public", "expansionsList.json.local"), JSON.stringify(idStrings));
