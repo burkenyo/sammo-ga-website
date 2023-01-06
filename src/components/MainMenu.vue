76f8c2d79f8db24a02cf4c01602c303a2471a527
Sammo Gabay <sammolito@gmail.com>
2023-01-05 19:41:07 -0500

    Add fun little scripty to update my name as seen in the main menu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { useNameUpdater } from '@/nameUpdater';
+import { onBeforeUnmount, ref } from 'vue';
 
 const menuRoutes = useRouter()
   .getRoutes()
   .filter(r => Number.isInteger(r.meta.menuOrder))
   .sort((a, b) => (a.meta.menuOrder as number) - (b.meta.menuOrder as number));
+
+const myName = ref("");
+
+const canceler = useNameUpdater(myName);
+
+onBeforeUnmount(canceler);
 </script>
 
 <template>
-  <h1>Sammo Gabay</h1>
+  <h1>{{ myName }}</h1>
   <nav>
     <span class="menu" v-for="route in menuRoutes" :key="route.meta.menuOrder as number">
       <template v-if="route.meta.menuOrder == $route.meta.menuOrder">{{ $route.meta.title }}</template>
